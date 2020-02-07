fe5f55cd13b38b7e00d9c24933520bd7381368b3
nttr-kurisima <kurisima@nttr-tech.co.jp>
2020-02-07 10:43:31 +0900

    Add iOS Browser test script

---
+++ appium/Python/iosBrowserTest.py
@@ -0,0 +1,58 @@
+import os
+import sys
+import unittest
+
+from time import sleep
+from appium import webdriver
+
+# get userName, password from Environment variable
+RTK_USERNAME = os.environ.get('RTK_USERNAME')
+RTK_PASSWORD = os.environ.get('RTK_PASSWORD')
+if not RTK_USERNAME or not RTK_PASSWORD:
+    print("Environment variable error")
+    sys.exit()
+
+
+class OpenUrlTest(unittest.TestCase):
+    def setUp(self):
+        caps = {
+            'userName': RTK_USERNAME,
+            'password': RTK_PASSWORD,
+            'logLevel': 'info',
+            'platformName': 'iOS',
+            'deviceName': 'iPhone 7.*',
+            'platformVersion': '11',
+            'browserName': 'Safari',
+        }
+        self.driver = webdriver.Remote('https://gwjp.appkitbox.com/wd/hub', caps)
+        print(self.driver)
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_google_search(self):
+        # Open URL
+        url = "https://www.google.com/"
+        print("Open URL: " + url)
+        self.driver.get(url)
+        element = self.driver.find_element_by_name('q')
+        sleep(5)
+        self.driver.save_screenshot('capture_01.png')
+
+        # Input keys
+        word = "Remote testKit"
+        print("Input Keys: " + word)
+        element.send_keys(word)
+        element.submit
+        sleep(5)
+        self.driver.save_screenshot('capture_02.png')
+
+        # Get value
+        value = self.driver.find_element_by_name('q').get_attribute('value')
+        print("Text field value=" + value)
+        self.assertEqual(value, "Remote testKit")
+
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(OpenUrlTest)
+    unittest.TextTestRunner(verbosity=2).run(suite)
