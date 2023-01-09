727d67a23a5a9a1ee70c1929ff9572b8e6748ed9
hirokazu-nagatsuma <nagatsuma@nttr-tech.co.jp>
2023-01-10 08:51:16 +0900

    test:動作の邪魔をしてたあまり意味のないputsコマンドを削除、またcapabilitiesの更新（最新の本番での動作や今後の2.0対応を踏まえての修正）

--- appium/Ruby/iosCalcTest.rb
+++ appium/Ruby/iosCalcTest.rb
@@ -25,8 +25,9 @@ class ContactsAndroidTests < Test::Unit::TestCase
         password: RTK_PASSWORD,
         deviceName: 'iPhone 7.*',
         platformName: 'iOS',
-        platformVersion: '11',
-        bundleId: 'com.apple.calculator'
+        platformVersion: '12',
+        bundleId: 'com.apple.calculator',
+        automationName: 'XCUITest'
       },
       appium_lib: {
         server_url: 'https://gwjp.appkitbox.com/wd/hub',
@@ -45,25 +46,21 @@ class ContactsAndroidTests < Test::Unit::TestCase
     @driver.save_screenshot('capture_01.png')
 
     el1 = @driver.find_element(:accessibility_id, '1')
-    puts el1
     el1.click
     @driver.save_screenshot('capture_02.png')
 
     is_lang_english = true
     begin
       el2 = @driver.find_element(:accessibility_id, 'multiply')
-      puts el2
       el2.click
     rescue StandardError
       is_lang_english = false
       el2 = @driver.find_element(:accessibility_id, '乗算')
-      puts el2
       el2.click
     end
     @driver.save_screenshot('capture_03.png')
 
     el3 = @driver.find_element(:accessibility_id, '3')
-    puts el3
     el3.click
     @driver.save_screenshot('capture_04.png')
 
@@ -75,36 +72,30 @@ class ContactsAndroidTests < Test::Unit::TestCase
 
     if is_lang_english
       el4 = @driver.find_element(:accessibility_id, 'equals')
-      puts el4
       el4.click
       @driver.save_screenshot('capture_07.png')
 
       el5 = @driver.find_element(:accessibility_id, 'Result')
-      puts el5
       value = el5.attribute('value')
 
       puts 'Text field value=' + value
       assert_equal(value, '9')
 
       el6 = @driver.find_element(:accessibility_id, 'clear')
-      puts el6
       el6.click
       @driver.save_screenshot('capture_08.png')
 
     else
       el4 = @driver.find_element(:accessibility_id, '計算実行')
-      puts el4
       el4.click
 
       el5 = @driver.find_element(:accessibility_id, '結果')
-      puts el5
       value = el5.attribute('value')
 
       puts 'Text field value=' + value
       assert_equal(value, '9')
 
       el6 = @driver.find_element(:accessibility_id, '消去')
-      puts el6
       el6.click
       @driver.save_screenshot('capture_08.png')
     end
